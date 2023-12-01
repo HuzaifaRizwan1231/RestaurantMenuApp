6fe1dc6c34d679697ac31a34f3ef3e966074618e
HuzaifaRizwan1231 <huzaifa.rizwan1231@gmail.com>
2023-12-02 01:29:40 +0500

    Saved AboutUs

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,7 +5,7 @@ export default function AboutUs() {
    <>
         <div className="container">
             <h1 className="text-center display-6 my-3"><b>ABOUT US</b></h1>
-            <div className="container AboutUs mb-4"></div>
+            <div className="container AboutUs mb-4  "></div>
             <div className="container AboutUs mb-5"></div>
         </div>
     

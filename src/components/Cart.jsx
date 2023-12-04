f1ac27d6c420d4da42a24df91bf8950b3b523424
HuzaifaRizwan1231 <huzaifa.rizwan1231@gmail.com>
2023-12-04 22:15:58 +0500

    Designed Cart Page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,134 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 export default function Cart() {
+  const [Quantity, setQuantity] = useState(1);
+
+  const HandleIncreaseQuantity = () => {
+    setQuantity(Quantity + 1);
+  };
+
+  const HandleDecreaseQuantity = () => {
+    if (Quantity > 1) {
+      setQuantity(Quantity - 1);
+    }
+  };
   return (
-    <div>Cart</div>
-  )
+    <>
+      <div className="container mt-4">
+        <h1 className="text-center display-6 mb-4">
+          <b>CART</b>
+        </h1>
+        <div className="container  mb-4">
+          <div className="card cardContainer">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-5 text-center ">
+                  <img
+                    src="/images/burger.png"
+                    className="card-img-top foodImage"
+                    alt="food"
+                  />
+                </div>
+                <div className="col-7 my-3 TopDealsCard">
+                  <h1 className="foodName mb-3">
+                    <b>HEAVY ZINGER</b>
+                  </h1>
+
+                  <h1 className="foodPrice mb-3">
+                    <b>Rs. 650</b>
+                  </h1>
+
+                  <div className="row mx-auto text-center" >
+                    <div className="col-5 btnQuantityCart">
+                      <b>
+                        <button
+                          onClick={HandleIncreaseQuantity}
+                          className=" "
+                        >
+                          +
+                        </button>
+                      </b>
+                    </div>
+                    <div className="col-2 m-auto p-0">
+                      <b>{Quantity}</b>
+                    </div>
+                    <div className="col-5 btnQuantityCart">
+                      <b>
+
+                      <button
+                        onClick={HandleDecreaseQuantity}
+                        className=" "
+                      >
+                        -
+                      </button>
+                      </b>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container  mb-4">
+          <div className="card cardContainer">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-5 text-center ">
+                  <img
+                    src="/images/burger.png"
+                    className="card-img-top foodImage"
+                    alt="food"
+                  />
+                </div>
+                <div className="col-7 my-3 TopDealsCard">
+                  <h1 className="foodName mb-3">
+                    <b>HEAVY ZINGER</b>
+                  </h1>
+
+                  <h1 className="foodPrice mb-3">
+                    <b>Rs. 650</b>
+                  </h1>
+
+                  <div className="row mx-auto text-center" >
+                    <div className="col-5 btnQuantityCart">
+                      <b>
+                        <button
+                          onClick={HandleIncreaseQuantity}
+                          className=" "
+                        >
+                          +
+                        </button>
+                      </b>
+                    </div>
+                    <div className="col-2 m-auto p-0">
+                      <b>{Quantity}</b>
+                    </div>
+                    <div className="col-5 btnQuantityCart">
+                      <b>
+
+                      <button
+                        onClick={HandleDecreaseQuantity}
+                        className=" "
+                      >
+                        -
+                      </button>
+                      </b>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container text-center mt-5">
+              <button
+                type="submit"
+                className="LoginButton w-75 text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Checkout
+              </button>
+              </div>
+      </div>
+    </>
+  );
 }

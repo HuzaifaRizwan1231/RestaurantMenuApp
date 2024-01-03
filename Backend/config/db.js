a825fe056cdb7ec975b52dc0f61f31214477c122
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-01-03 13:51:01 +0500

    db

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,18 +2,28 @@
 const mysql = require('mysql')
 
 const db = mysql.createConnection({
+    connectionLimit : 100,
+    waitForConnections : true,
+    queueLimit :0,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE 
+    database: process.env.DB_DATABASE,
+    debug    :  true,
+    wait_timeout : 28800,
+    connect_timeout :10
 })
 // const db = mysql.createPool({
-//     host: "127.0.0.1",
-//     user: "root",
-//     port: '3306',
-//     password: "",
-//     connectionLimit: 10,
-//     database: "rfc-project" 
+//     connectionLimit : 100,
+//     waitForConnections : true,
+//     queueLimit :0,
+//     host     : 'myremotehost',
+//     user     : '',
+//     password : '',
+//     database : 'mother51_moastage',
+//     debug    :  true,
+//     wait_timeout : 28800,
+//     connect_timeout :10
 // })
 
 

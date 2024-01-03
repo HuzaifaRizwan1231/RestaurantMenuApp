fd8cc0c200c2a761fdba7065e90fb50c5b858874
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-01-03 13:47:31 +0500

    env

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,20 +1,20 @@
 
 const mysql = require('mysql')
 
-// const db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE 
-// })
-const db = mysql.createPool({
-    host: "127.0.0.1",
-    user: "root",
-    port: '3306',
-    password: "",
-    connectionLimit: 10,
-    database: "rfc-project" 
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE 
 })
+// const db = mysql.createPool({
+//     host: "127.0.0.1",
+//     user: "root",
+//     port: '3306',
+//     password: "",
+//     connectionLimit: 10,
+//     database: "rfc-project" 
+// })
 
 
 module.exports = db;

2690c4f5ef8eb093c748c9d49730e9262f4b00fa
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-01-03 15:53:30 +0500

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,8 +10,8 @@ export default function Footer() {
     
   return (
     <>
-      <footer className=" dark:bg-gray-900 mt-48 sm:text-left text-center">
-        <div className="footer mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+      <footer className="footer dark:bg-gray-900 mt-48 sm:text-left text-center">
+        <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
               <Link  to="/" onClick={HandleScroll} className="flex items-center">

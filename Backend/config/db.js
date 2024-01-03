1408e8d800629783029788b9a9182641548a339d
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-01-03 13:45:46 +0500

    commit

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -7,10 +7,12 @@ const mysql = require('mysql')
 //     password: process.env.DB_PASSWORD,
 //     database: process.env.DB_DATABASE 
 // })
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: "127.0.0.1",
     user: "root",
+    port: '3306',
     password: "",
+    connectionLimit: 10,
     database: "rfc-project" 
 })
 
